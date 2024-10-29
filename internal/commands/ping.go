d433c83c0e4159febd2e12fadb028cad40ad2943
ruriyoshinova <github@ruriyoshinova.dev>
2024-10-29 13:22:28 +0800

    🦄 refactor: remove code block

--- internal/commands/ping.go
+++ internal/commands/ping.go
@@ -16,14 +16,14 @@ func Ping(s *discordgo.Session, m *discordgo.MessageCreate) {
 	for i := 0; i < iterations; i++ {
 		start := time.Now()
 		s.ChannelMessageEdit(m.ChannelID, msg.ID, fmt.Sprintf(
-			"Pinging... (Avg: `%.2f` ms)",
+			"Pinging... (Avg: %.2f ms)",
 			safeArg(totalLatency, i)))
 		latency := time.Since(start)
 		totalLatency += latency
 	}
 
 	avgLatency := float64(totalLatency) / float64(iterations) / float64(time.Millisecond)
-	s.ChannelMessageEdit(m.ChannelID, msg.ID, fmt.Sprintf("Pong! (Avg: `%.2f` ms)", avgLatency))
+	s.ChannelMessageEdit(m.ChannelID, msg.ID, fmt.Sprintf("Pong! (Avg: %.2f ms)", avgLatency))
 }
 
 func safeArg(totalLatency time.Duration, i int) float64 {
